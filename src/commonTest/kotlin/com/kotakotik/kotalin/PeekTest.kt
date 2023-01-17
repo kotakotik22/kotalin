a46776dcde8191706cad8da7b1643186cec9be2c
kotakotik22 <61428759+kotakotik22@users.noreply.github.com>
2023-01-17 17:14:59 +0100

    tests

---
+++ src/commonTest/kotlin/com/kotakotik/kotalin/PeekTest.kt
@@ -0,0 +1,30 @@
+package com.kotakotik.kotalin
+
+import com.kotakotik.kotalin.peeking.peeking
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+
+class PeekTest {
+    @Test
+    fun test() {
+        val iter = listOf("a", "b", "c").iterator().peeking()
+        assertEquals("a", iter.peek())
+        assertEquals("a", iter.next())
+        assertEquals("b", iter.peek())
+        assertEquals("b", iter.next())
+        assertEquals("c", iter.next())
+        assertFalse(iter.hasNext())
+    }
+
+    @Test
+    fun charTest() {
+        val iter = "abc".iterator().peeking()
+        assertEquals('a', iter.peek())
+        assertEquals('a', iter.next())
+        assertEquals('b', iter.peek())
+        assertEquals('b', iter.next())
+        assertEquals('c', iter.next())
+        assertFalse(iter.hasNext())
+    }
+}
